0e1c6361929e5440ee148fd351cb64199a0877db
Martin Müller <mmueller@ualberta.ca>
2024-09-20 15:57:11 -0600

    rename game_move_generator to move_generator

--- game.h
+++ game.h
@@ -7,25 +7,23 @@
 class game
 {
 public:
-    game() = 0; // empty game
-    game(std::string game_as_string) = 0;
+//     game() = 0; // empty game
+//     game(std::string game_as_string) = 0;
     bool solve();
 }; // game
 
 
 std::ostream& operator<<(std::ostream& out, const game& g);
 
-class game_move_generator
+//---------------------------------------------------------------------------
+class move_generator
 {
 public:
-    game_move_generator(const game& g, int color) = 0;
-    void operator++() = 0;
-    operator bool() const = 0;
+//     move_generator(const game& g, int color) = 0;
+    virtual void operator++() = 0;
+    virtual operator bool() const = 0;
 
 private:
-    const game& _g;
-    int _color;
-    int _current;
-}; // game_move_generator
+}; // move_generator
 
 #endif // game_H
