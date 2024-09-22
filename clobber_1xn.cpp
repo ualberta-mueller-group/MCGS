a55ed10d4c048bab80d7bf4406fd48423e9843c5
Martin Müller <mmueller@ualberta.ca>
2024-09-22 17:38:54 -0600

    start implementing clobber move generator - not working yet.

--- clobber_1xn.cpp
+++ clobber_1xn.cpp
@@ -32,13 +32,102 @@ void clobber_1xn::undo_move()
     replace(to, opponent());
 }
 
-move_generator* clobber_1xn::create_mg() const
-{
-    return nullptr;
-}
 
 std::ostream& operator<<(std::ostream& out, const clobber_1xn& g)
 {
     out << g.board_as_string();
     return out;
 }
+//---------------------------------------------------------------------------
+
+class clobber_1xn_move_generator : public move_generator
+{
+public:
+    clobber_1xn_move_generator(const clobber_1xn& game);
+    void operator++();
+    operator bool() const;
+    move gen_move() const;
+private:
+    int at(int p) const {return _game.at(p); }
+    int to_play() const {return _game.to_play(); }
+    int opponent() const {return _game.opponent(); }
+    bool is_move(int p, int dir) const;
+    bool has_move(int p) const;
+
+    void find_next_move();
+    const clobber_1xn& _game;
+    int _current; // current stone location to test
+    int _dir; // +-1
+};
+
+inline clobber_1xn_move_generator::clobber_1xn_move_generator(const clobber_1xn& game) :
+    _game(game), _current(0), _dir(1)
+{
+    find_next_move();
+}
+
+
+
+void clobber_1xn_move_generator::operator++()
+{
+    find_next_move();
+}
+
+inline bool clobber_1xn_move_generator::is_move(int p, int dir) const
+{
+    return at(p + dir) == opponent();
+}
+
+bool clobber_1xn_move_generator::has_move(int p) const
+{
+    assert(at(p) == to_play());
+    return is_move(p, 1) || is_move(p, -1);
+}
+
+void clobber_1xn_move_generator::find_next_move()
+{
+    const int num = _game.size();
+    
+    // try other dir first.
+    if (   _current < num
+           && _dir == 1
+           &&  at(_current) == to_play()
+           && is_move(_current, -1)
+       )
+       _dir = -1;
+    
+    else // advance
+    {
+        while (   _current < num
+               && (  at(_current) != to_play()
+                  || ! has_move(_current)
+                  )
+              )
+            ++_current;
+        if (_current < num)
+        {
+            if (is_move(_current, 1))
+               _dir = 1;
+            else
+               _dir = -1;
+        }
+    }
+}
+
+clobber_1xn_move_generator::operator bool() const
+{
+    return _current < _game.size();
+}
+
+move clobber_1xn_move_generator::gen_move() const
+{
+    assert(operator bool());
+    return cgt_move::two_part_move(_current, _current + _dir);
+}
+//---------------------------------------------------------------------------
+
+move_generator* clobber_1xn::create_mg() const
+{
+    return new clobber_1xn_move_generator(*this);
+}
+//---------------------------------------------------------------------------
