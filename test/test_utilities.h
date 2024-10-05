d4670c13f778a25e8885e25391757238db09513e
Martin Müller <mmueller@ualberta.ca>
2024-10-04 18:37:07 -0600

    assert_move for simple int; rename old assert_move to assert_two_part_move; add assert_solve

--- test/test_utilities.h
+++ test/test_utilities.h
@@ -8,11 +8,24 @@
 #include "cgt_move.h"
 #include "game.h"
 
-inline void assert_move(move_generator& mg, int from, int to)
+inline void assert_move(move_generator& mg, int mv)
+{ 
+    const move m = mg.gen_move();
+    assert(m == mv);
+}
+
+inline void assert_two_part_move(move_generator& mg, int from, int to)
 { 
     move m = mg.gen_move();
     assert(from == cgt_move::from(m));
     assert(to == cgt_move::to(m));
+}
+
+inline void assert_solve(game& pos, bw to_play, bool expected_result)
+{
+    alternating_move_game g(pos, to_play);
+    const bool result = g.solve();
+    assert(result == expected_result);
 }
 
 #endif // test_utilities_H
