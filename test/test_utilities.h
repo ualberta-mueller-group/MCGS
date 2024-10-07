1d631e222b415136d8203ac89d338032f3838010
Martin Müller <mmueller@ualberta.ca>
2024-10-07 12:03:25 -0600

    more test utilities

--- test/test_utilities.h
+++ test/test_utilities.h
@@ -5,13 +5,35 @@
 #ifndef test_utilities_H
 #define test_utilities_H
 
+#include <iostream>
 #include "cgt_move.h"
 #include "game.h"
 
+using std::cout;
+using std::endl;
+
+inline void assert_equal(int a, int b)
+{ 
+    if (!(a==b))
+        cout << "assert_equal FAIL: " << a << ' ' << b << endl;
+    assert(a == b);
+}
 inline void assert_move(move_generator& mg, int mv)
 { 
     const move m = mg.gen_move();
     assert(m == mv);
+}
+
+inline void assert_num_moves(const game& g, bw to_play, int num_moves)
+{ 
+    std::unique_ptr<move_generator>mgp(g.create_move_generator(to_play));
+    move_generator& mg(*mgp);
+    for (int i = 0; i < num_moves; ++i)
+    {
+        assert(mg);
+        ++mg;
+    }
+    assert(!mg);
 }
 
 inline void assert_two_part_move(move_generator& mg, int from, int to)
@@ -23,6 +45,7 @@ inline void assert_two_part_move(move_generator& mg, int from, int to)
 
 inline void assert_solve(game& pos, bw to_play, bool expected_result)
 {
+    assert_black_white(to_play);
     alternating_move_game g(pos, to_play);
     const bool result = g.solve();
     assert(result == expected_result);
