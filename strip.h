cd9ffe488bbf662e00fc8212fdd50fffd6eaafbb
Henry Du <du2@ualberta.ca>
2024-10-21 14:48:37 -0600

    Change back to private _board

--- strip.h
+++ strip.h
@@ -26,7 +26,7 @@ public:
     // Less checking than play_stone or remove_stone
     void replace(int p, int color);
     std::string board_as_string() const;
-protected:
+private:
     vector<int> _board; // todo try char as well.
 };
 
