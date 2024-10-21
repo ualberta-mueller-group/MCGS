4feb16bd7c9babf535a5f0cd1d891438a9f71429
Martin Müller <mmueller@ualberta.ca>
2024-10-21 17:27:04 -0600

    Move minimax solve() into alternating_move_game

--- alternating_move_game.cpp
+++ alternating_move_game.cpp
@@ -3,9 +3,26 @@
 //---------------------------------------------------------------------------
 #include "alternating_move_game.h"
 
-#include "solve.h"
+#include <memory>
 
+// Solve combinatorial game - find winner
+// Game-independent implementation of basic boolean minimax
 bool alternating_move_game::solve()
 {
-    return ::solve(*this);
+    game& pos = game_pos();
+    std::unique_ptr<move_generator>mgp(pos.create_move_generator(to_play()));
+    move_generator& mg = *mgp;
+    
+    for (; mg; ++mg)
+    {
+        play(mg.gen_move());
+        bool success = false;
+        bool found = find_static_winner(success);
+        if (! found)
+            success = not solve();
+        undo_move();
+        if (success)
+            return true;
+    }
+    return false;
 }
