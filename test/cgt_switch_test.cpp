22b44c8692742748bcead8150d658f6a92307668
Martin Müller <mmueller@ualberta.ca>
2024-10-24 10:22:35 -0600

    play and undo tests for switch_game

--- test/cgt_switch_test.cpp
+++ test/cgt_switch_test.cpp
@@ -8,6 +8,31 @@
 
 namespace cgt_switch_test{
 
+void play_undo1()
+{
+    switch_game g(5, 3);
+    assert(!g.is_integer());
+    g.play(1, WHITE);
+    assert(g.is_integer());
+    assert(g.value() == 3);
+    g.undo_move();
+    assert(!g.is_integer());
+}
+
+void play_undo2()
+{
+    switch_game g(5, 3);
+    g.play(1, WHITE);
+    assert(g.value() == 3);
+    g.play(0, BLACK);
+    assert(g.is_integer());
+    assert(g.value() == 2);
+    g.undo_move();
+    assert(g.value() == 3);
+    g.undo_move();
+    assert(!g.is_integer());
+}
+
 void test1()
 {
     switch_game g(3, -1);
@@ -26,13 +51,13 @@ void test3()
 {
     switch_game g(5, 3);
     assert_solve(g, BLACK, true);
-    // TODO assert_solve(g, WHITE, false);
+    assert_solve(g, WHITE, false);
 }
 
 void test4()
 {
     switch_game g(-5, -13);
-    // TODO assert_solve(g, BLACK, false);
+    assert_solve(g, BLACK, false);
     assert_solve(g, WHITE, true);
 }
 
@@ -74,6 +99,8 @@ void test4()
 
 void cgt_switch_test_all()
 {
+    cgt_switch_test::play_undo1();
+    cgt_switch_test::play_undo2();
     cgt_switch_test::test1();
     cgt_switch_test::test2();
     cgt_switch_test::test3();
