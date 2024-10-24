ac521ea614352d04b5cfc7ab77c3472c63ac1fd3
Martin Müller <mmueller@ualberta.ca>
2024-10-24 10:21:27 -0600

    Fully implement switch, also for integer subgames

--- cgt_switch.cpp
+++ cgt_switch.cpp
@@ -1,22 +1,48 @@
 //---------------------------------------------------------------------------
 // Simple combinatorial games - switches
+// Games of the for { l | r }, with integers l > r.
 //---------------------------------------------------------------------------
 #include "cgt_switch.h"
 
+#include "cgt_integer_game.h"
+#include "cgt_move.h"
+
+
 #include <iostream>
-using std::cout, std::endl;
+using std::cout;
+using std::endl;
+
+const int SWITCH_MOVE_CODE = 1;
 
 void switch_game::play(const move& m, bw to_play)
 {
-    assert(!_is_integer);
-    _is_integer = true;
-    _int_value = to_play == BLACK ? _left : _right;
+    if (_is_integer)
+    {
+        assert(m == INTEGER_MOVE_CODE);
+        _int_game.play(m, to_play);
+    }
+    else
+    {
+        assert(m == SWITCH_MOVE_CODE);
+        _is_integer = true;
+        _int_game.set_value(to_play == BLACK ? _left : _right);
+    }
     game::play(m, to_play);
 }
 
 void switch_game::undo_move()
 {
-    _is_integer = false;
+    assert(_is_integer);
+    const int m = cgt_move::decode(last_move());
+    if (m == SWITCH_MOVE_CODE) // back from integer to switch
+    {
+        _is_integer = false;
+    }
+    else
+    {
+        assert(m == INTEGER_MOVE_CODE);
+        _int_game.undo_move();
+    }
     game::undo_move();
 }
 
@@ -34,8 +60,10 @@ private:
 
 switch_move_generator::switch_move_generator(const switch_game& game, bw to_play)
     : move_generator(to_play),
-      _generated(game.is_integer())
-{ }
+      _generated(false)
+{ 
+    assert(!game.is_integer());
+}
 
 void switch_move_generator::operator++()
 {
@@ -47,16 +75,19 @@ switch_move_generator::operator bool() const
     return !_generated;
 }
 
-
 move switch_move_generator::gen_move() const
 {
-    return 0;
+    assert(!_generated);
+    return SWITCH_MOVE_CODE;
 }
 
 //---------------------------------------------------------------------------
 move_generator* switch_game::create_move_generator(bw to_play) const
 {
-    return new switch_move_generator(*this, to_play);
+    if (_is_integer)
+        return _int_game.create_move_generator(to_play);
+    else
+        return new switch_move_generator(*this, to_play);
 }
 //---------------------------------------------------------------------------
 
--- cgt_switch.h
+++ cgt_switch.h
@@ -5,18 +5,15 @@
 #define cgt_switch_H
 
 #include "cgt_basics.h"
+#include "cgt_integer_game.h"
 #include "game.h"
 
 //---------------------------------------------------------------------------
 
-// TODO incomplete implementation - can ONLY play the first move,
-// cannot play out the resulting integer game.
-// This will be much simpler to implement with new sum game play()
-// which can return new game(s) of other type(s)
 class switch_game : public game
 {
 public:
-    switch_game(int left, int right); // TODO could be fractions?
+    switch_game(int left, int right);
     void play(const move& m, bw to_play);
     void undo_move();
     move_generator* create_move_generator(bw to_play) const;
@@ -27,20 +24,20 @@ public:
     int value() const
     {
         assert(is_integer());
-        return _int_value; 
+        return _int_game.value();
     }
 
 private:
     const int _left, _right;
     bool _is_integer; // has root move been played?
-    int _int_value; //only meaningful if _is_integer
+    integer_game _int_game; //only meaningful if _is_integer
 };
 
 inline switch_game::switch_game(int left, int right) : 
     _left(left), 
     _right(right),
     _is_integer(false),
-    _int_value(0)
+    _int_game(0)
 {
     assert(left > right);
 }
