f577a1bb0e8ea1b81e3a921e54623656703acc51
tfolkersen <folkerse@ualberta.ca>
2024-12-17 21:15:19 -0700

    switch sumgame tests

--- test/sumgame_test_switch.cpp
+++ test/sumgame_test_switch.cpp
@@ -4,38 +4,58 @@
 
 void switch1()
 {
+    // positive
     assert_sum_outcomes(true, false, 
         int2_spec<switch_game>(5, 3)
     );
-
 }
 
 void switch2()
 {
-
+    // negative
+    assert_sum_outcomes(false, true, 
+        int2_spec<switch_game>(-5, -7)
+    );
 }
 
 void switch3()
 {
-
+    // zero
+    assert_sum_outcomes(false, false, 
+        int2_spec<switch_game>(30, 10),
+        int2_spec<switch_game>(-10, -30)
+    );
 }
 
 void switch4()
 {
-
+    // positive
+    assert_sum_outcomes(true, false, 
+        int2_spec<switch_game>(5, 4),
+        int2_spec<switch_game>(2, -1),
+        int2_spec<switch_game>(-1, -3),
+        int2_spec<switch_game>(1, -1)
+    );
 }
 
 void switch5()
 {
-
+    // N position
+    assert_sum_outcomes(true, true,
+        int2_spec<switch_game>(10, -10)
+    );
 }
 
 void switch6()
 {
-
+    // N position
+    assert_sum_outcomes(true, true,
+        int2_spec<switch_game>(5, -4),
+        int2_spec<switch_game>(1, -1),
+        int2_spec<switch_game>(20, 5),
+        int2_spec<switch_game>(-10, -20)
+    );
 }
-
-
 
 void sumgame_test_switch_all()
 {
--- test/test_utilities.h
+++ test/test_utilities.h
@@ -6,6 +6,7 @@
 // simpler include guard
 
 #include <memory>
+#include <tuple>
 #include "alternating_move_game.h"
 #include "cgt_move.h"
 #include "game.h"
