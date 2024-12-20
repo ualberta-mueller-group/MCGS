15ced0abc0dff3403024769124b5baa318e23bd5
tfolkersen <folkerse@ualberta.ca>
2024-12-19 17:14:12 -0700

    rewrite parser

--- parser.cpp
+++ parser.cpp
@@ -5,19 +5,30 @@
 
 using std::string, std::ifstream, std::cout, std::cin, std::endl;
 
-bool token_is_format(const string& token, const char& open, const char& close)
-{
-    int N = token.size();
+/*
+    (clobber_1xn) <-- game title
+    {B win} <-- command
 
-    if (N <= 0 || token[0] != open || token.back() != close)
+*/
+
+bool is_enclosed_format(const string& token, const char& open, const char& close)
+{
+    if (token.size() < 2)
     {
         return false;
     }
 
+    if (token[0] != open || token.back() != close)
+    {
+        return false;
+    }
+
+    int N = token.size();
     for (int i = 1; i < N - 1; i++)
     {
-        const char c = token[i];
+        const char& c = token[i];
 
+        // TODO check for reserved characters instead
         if (c == open || c == close)
         {
             return false;
@@ -27,40 +38,33 @@ bool token_is_format(const string& token, const char& open, const char& close)
     return true;
 }
 
-string get_format(string chunk, ifstream& stream, const char& open, const char& close)
+bool get_enclosed(ifstream& stream, string& token, const char& open, const char& close)
 {
-    if (chunk.back() == close)
+    if (token[0] != open)
     {
-        assert(token_is_format(chunk, open, close));
-        return chunk;
+        return false;
     }
 
-    string token;
-    while (stream >> token)
+    if (is_enclosed_format(token, open, close))
     {
-        chunk += " " + token;
-        if (chunk.back() == close)
+        return true;
+    }
+
+    string new_token;
+    while (stream >> new_token)
+    {
+        token += " " + new_token;
+
+        if (is_enclosed_format(token, open, close))
         {
-            break;
+            return true;
         }
+
     }
 
-    if (chunk.back() == close)
-    {
-        assert(token_is_format(chunk, open, close));
-        return chunk;
-    }
-
-    assert(false);
-    return "";
+    return false;
 }
 
-void strip_title(string& title)
-{
-    assert(title.size() >= 2);
-    title.pop_back();
-    title = title.substr(1);
-}
 
 void parse(const string& file_name)
 {
@@ -68,50 +72,75 @@ void parse(const string& file_name)
 
     if (!stream.is_open())
     {
-        cout << "Failed to open" << endl;
+        cout << "Failed to open file: " << file_name << endl;
         return;
     }
 
-
     string token;
-    string chunk;
-
-    string current_title;
-
     while (stream >> token)
     {
-        // Match macro
+        // Match command
         if (token[0] == '{')
         {
-            chunk = get_format(token, stream, '{', '}');
-            cout << "GOT MACRO: " << chunk << endl;
+            bool success =  get_enclosed(stream, token, '{', '}');
+
+            if (!success)
+            {
+                cout << "Parser error on line ?: Failed to match command" << endl;
+                return;
+            }
+
+            cout << "Got command: " << token << endl;
             continue;
         }
 
         // Match title
         if (token[0] == '(')
         {
-            chunk = token;
-            assert(token_is_format(token, '(', ')'));
-            strip_title(chunk);
-            cout << "GOT TITLE: " << chunk << endl;
-            current_title = chunk;
+            bool success = get_enclosed(stream, token, '(', ')');
+
+            if (!success)
+            {
+                cout << "Parser error on line ?: Failed to match game title" << endl;
+                return;
+            }
+
+            cout << "Got game title: " << token << endl;
             continue;
         }
 
-
-        // Match quote
-        if (token[0] == '\"')
+        // Match quotes
+        if (token[0] == '"')
         {
-            chunk = get_format(token, stream, '\"', '\"');
-            cout << "GOT QUOTE: " << chunk << endl;
+            bool success = get_enclosed(stream, token, '"', '"');
+
+            if (!success)
+            {
+                cout << "Parser error on line ?: Failed to match quotes" << endl;
+                return;
+            }
+
+            cout << "Got quotes: " << token << endl;
             continue;
         }
 
-        // Token must be a game_token
-        cout << "GOT GAME TOKEN: " << token << endl;
+        // Match comment
+        if (token[0] == '/')
+        {
+            bool success = get_enclosed(stream, token, '/', '/');
+
+            if (!success)
+            {
+                cout << "Parser error on line ?: Failed to match comment" << endl;
+            }
+
+            cout << "Got comment: " << token << endl;
+            continue;
+        }
+
+        // Must be game token
+        cout << "Got simple token: " << token << endl;
 
     }
 
 }
-
