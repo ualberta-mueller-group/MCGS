7ba325510699793381258c38f9298ac7ee9ea47c
tfolkersen <folkerse@ualberta.ca>
2025-01-15 03:31:18 -0700

    Better table generation, diff mode, better HTML template

--- table-template.js
+++ table-template.js
@@ -1,7 +1,13 @@
+let g_mode = "";
+let g_filterText = "";
+let g_include = true;
+let g_regex = false;
+let g_searchColumn = -1;
+
 
 
 // Hide or show certain rows of the table
-function setTableFilter(table, mode) {
+function setTableFilter() {
 
     // Hide or show rows having at least one given class
     const showByClass = (classes, show) => {
@@ -15,38 +21,129 @@ function setTableFilter(table, mode) {
     };
 
 
-    if (mode == "all") {
+    if (g_mode == "all") {
         showByClass(["row"], true);
     }
 
-    if (mode == "fail") {
+    if (g_mode == "fail") {
         showByClass(["row"], false);
         showByClass(["row-fail"], true);
     }
 
-    if (mode == "timeout") {
+    if (g_mode == "timeout") {
         showByClass(["row"], false);
         showByClass(["row-timeout"], true);
     }
 
-    if (mode == "bad") {
+    if (g_mode == "bad") {
         showByClass(["row"], false);
         showByClass(["row-fail"], true);
         showByClass(["row-timeout"], true);
     }
 }
 
+function setTableFilterText() { 
+    const table = document.getElementById("data-table");
+    const rows = table.rows;
+
+    let matchFunction;
+
+    if (g_regex) {
+        const re = new RegExp(g_filterText);
+
+        matchFunction = (text) => {
+            return re.test(text)
+        };
+
+    } else {
+        matchFunction = (text) => {
+            return text.includes(g_filterText);
+        };
+    }
+
+
+    for (let i = 1; i < rows.length; i++) {
+        const row = rows[i];
+
+        if (row.hidden) {
+            continue;
+        }
+
+        const divs = row.getElementsByTagName("div");
+
+
+        let foundMatch = false;
+        if (g_searchColumn == -1) {
+            for (let i = 0; i < divs.length; i++) {
+                const divText = divs[i].innerHTML;
+
+                if (matchFunction(divText)) {
+                    foundMatch = true;
+                    break;
+                }
+            }
+        } else {
+            foundMatch = matchFunction(divs[g_searchColumn].innerHTML);
+        }
+
+        if (foundMatch != g_include) {
+            row.hidden = true;
+        }
+    }
+
+
+}
+
+function refresh() {
+    setTableFilter();
+    setTableFilterText();
+}
+
+
 
 // Wait for the document to finish loading before doing anything
 document.addEventListener("DOMContentLoaded", () => {
     const dropdown = document.getElementById("outcome-filter-dropdown");
-    const table = document.getElementById("data-table");
+    //const table = document.getElementById("data-table");
 
-    dropdown.addEventListener("change", (e) => {
-        const value = e.target.value;
-        setTableFilter(table, value);
+    const filterTextInput = document.getElementById("filter-text-input");
+    const filterTextExclude = document.getElementById("filter-text-exclude");
+    const filterTextRegex = document.getElementById("filter-text-regex");
+    const filterColumnSelect = document.getElementById("filter-text-column-select");
+
+    g_mode = dropdown.value;
+    g_filterText = filterTextInput.value;
+    g_include = !filterTextExclude.checked;
+    g_regex = filterTextRegex.checked;
+    g_searchColumn = filterColumnSelect.value;
+
+
+    filterTextInput.addEventListener("input", (e) => {
+        g_filterText = e.target.value;
+        refresh();
     });
 
-    setTableFilter(table, dropdown.value);
+    dropdown.addEventListener("change", (e) => {
+        g_mode = e.target.value;
+        refresh();
+    });
+
+    filterTextExclude.addEventListener("change", (e) => {
+        g_include = !e.target.checked;
+        refresh();
+    });
+
+    filterTextRegex.addEventListener("change", (e) => {
+        g_regex = e.target.checked;
+        refresh();
+    });
+
+    filterColumnSelect.addEventListener("change", (e) => {
+        g_searchColumn = e.target.value;
+        refresh();
+    });
+
+
+    refresh();
 
 });
