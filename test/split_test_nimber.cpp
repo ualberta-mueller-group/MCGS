8bdfa9a1a955993a73a653538804e1729f2d5da2
tfolkersen <folkerse@ualberta.ca>
2025-02-07 02:22:08 -0700

    add split tests for more non-splitting games

---
+++ test/split_test_nimber.cpp
@@ -0,0 +1,34 @@
+#include "split_test_nimber.h"
+#include "cgt_nimber.h"
+#include "split_test_utils.h"
+
+
+namespace {
+    void nimber1()
+    {
+        nimber g(5);
+        assert_no_split(&g);
+    }
+
+
+    void nimber2()
+    {
+        nimber g(0);
+        assert_no_split(&g);
+    }
+
+
+    void nimber3()
+    {
+        nimber g(21);
+        assert_no_split(&g);
+    }
+
+};
+
+void split_test_nimber_all()
+{
+    nimber1();
+    nimber2();
+    nimber3();
+}
