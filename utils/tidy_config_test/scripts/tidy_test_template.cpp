61c4be43664bb7502bff622bedfa9cd9982e9fea
tfolkersen <folkerse@ualberta.ca>
2025-02-08 00:01:42 -0700

    add more tests, change constexpr rules

--- utils/tidy_config_test/scripts/tidy_test_template.cpp
+++ utils/tidy_config_test/scripts/tidy_test_template.cpp
@@ -21,6 +21,66 @@ static int _CHECK = 0; // YESERROR
 static const int CHECK = 0; // NOERROR
 static const int _CHECK = 0; // YESERROR
 
+enum CHECK_ENUM // YESERROR
+{
+    check, // YESERROR
+};
+
+enum check_enum // NOERROR
+{
+    CHECK, // NOERROR
+};
+
+struct CHECK_STRUCT // YESERROR
+{
+};
+
+struct check_struct // NOERROR
+{
+};
+
+void CHECK_FUNCTION() // YESERROR
+{
+}
+
+void check_function() // NOERROR
+{
+}
+
+constexpr int check = 0; // YESERROR
+constexpr int CHECK = 0; // NOERROR
+
+constexpr int CHECK_FUNCTION() // YESERROR
+{
+    return 5;
+};
+
+
+constexpr int check_function() // NOERROR
+{
+    return 5;
+};
+
+void some_func(int x, const int CHECK) // YESERROR
+{
+}
+
+struct some_other_struct
+{
+public:
+    static constexpr int CHECK = 5; // NOERROR
+    static constexpr int check = 6; // YESERROR
+
+    constexpr int check() // NOERROR
+    {
+        return 5;
+    }
+
+    constexpr int CHECK() // YESERROR
+    {
+        return 5;
+    }
+};
 
 
 struct some_struct
