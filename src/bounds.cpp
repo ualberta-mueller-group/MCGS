643233367c78d72a5f37b39338f835280f84cbb3
tfolkersen <folkerse@ualberta.ca>
2025-02-11 23:51:20 -0700

    tie breaking, tie breaking prediction

--- src/bounds.cpp
+++ src/bounds.cpp
@@ -13,6 +13,9 @@
 
 using namespace std;
 
+int step_count = 0;
+int search_count = 0;
+
 
 //              0 0 0   ?   0 0 0
 
@@ -35,6 +38,18 @@ using namespace std;
 
 */
 
+enum optimization_level_enum
+{
+    OPT_NONE,
+
+    OPT_1_SIDE,
+
+    OPT_ASSUME_NEG,
+    OPT_ASSUME_POS,
+    OPT_2_SIDE,
+};
+
+
 
 enum search_result
 {
@@ -48,6 +63,8 @@ static_assert(RESULT_UNKNOWN != RESULT_TRUE);
 
 //const int RADIUS = 16000;
 const int RADIUS = 16000;
+optimization_level_enum OPTIMIZATION_LEVEL = OPT_NONE;
+
 const int MIN = -RADIUS;
 const int MAX = RADIUS;
 
@@ -146,18 +163,17 @@ game* get_inverse_scale_game(int virtual_idx)
 */
 
 
+const bool silent = true;
+
 
 void get_bounds(vector<game*>& games)
 {
-    int check_count = 0;
-    int search_count = 0;
-
     const int BOUND_UNDEFINED = std::numeric_limits<int>::min();
 
     int bound_low = BOUND_UNDEFINED;
     int bound_high = BOUND_UNDEFINED;
 
-    relation grid[DIAMETER];
+   relation grid[DIAMETER];
 
     for (int i = 0; i < DIAMETER; i++)
     {
@@ -189,6 +205,7 @@ void get_bounds(vector<game*>& games)
 
     auto step = [&](arena& ar) -> arena
     {
+        step_count++;
         arena split_arena = ARENA_INVALID;
 
         int& low = ar.first;
@@ -199,8 +216,10 @@ void get_bounds(vector<game*>& games)
         int virtual_i = (high + low) / 2;
         int real_i = virtual_to_real_idx(virtual_i);
 
-        cout << "Checking " << virtual_i << endl;
-        check_count++;
+        if (!silent)
+        {
+            cout << "Checking " << virtual_i << endl;
+        }
 
         game* inverse_scale_game = get_inverse_scale_game(virtual_i);
 
@@ -215,83 +234,46 @@ void get_bounds(vector<game*>& games)
 
 
         // BASIC APPROACH
-        /*
-        search_count += 2;
-        bool black_first = sum.solve();
-
-        sum.set_to_play(WHITE);
-        bool white_first = sum.solve();
-
-        // 0 0
-        // S - Gi = 0
-        // S = Gi
-        // Gi = S
-        if (!black_first && !white_first)
+        if (OPTIMIZATION_LEVEL == OPT_NONE)
         {
-            grid[real_i] = R_EQUAL; 
-        }
+            search_count += 2;
+            bool black_first = sum.solve();
 
-        // 0 1
-        // S - Gi < 0
-        // S < Gi
-        // Gi > S
-        if (!black_first && white_first)
-        {
-            grid[real_i] = R_GREATER; 
-            high = virtual_i - 1;
-
-            assert(bound_high == BOUND_UNDEFINED || virtual_i < bound_high);
-            bound_high = virtual_i;
-        }
-
-        // 1 0
-        // S - Gi > 0
-        // S > Gi
-        // Gi < S
-        if (black_first && !white_first)
-        {
-            grid[real_i] = R_LESS;
-            low = virtual_i + 1;
-
-            assert(bound_low == BOUND_UNDEFINED || virtual_i > bound_low);
-            bound_low = virtual_i;
-        }
-
-        // 1 1
-        // S - Gi ?= 0
-        // S ?= Gi
-        // Gi ?= S
-        if (black_first && white_first)
-        {
-            grid[real_i] = R_FUZZY;
-            split_arena = {virtual_i + 1, high};
-            high = virtual_i - 1;
-        }
-        */
-
-
-        // 1 SIDED OPTIMIZATION
-        /*
-        search_count++;
-        bool black_first = sum.solve();
-
-        // 0 ?
-        // S - Gi <= 0
-        // S <= Gi
-        // Gi >= S
-        if (!black_first)
-        {
-            grid[real_i] = R_GE; 
-            high = virtual_i - 1;
-
-            assert(bound_high == BOUND_UNDEFINED || virtual_i < bound_high);
-            bound_high = virtual_i;
-        } else
-        {
-
-            search_count++;
             sum.set_to_play(WHITE);
             bool white_first = sum.solve();
+
+            // 0 0
+            // S - Gi = 0
+            // S = Gi
+            // Gi = S
+            if (!black_first && !white_first)
+            {
+                grid[real_i] = R_EQUAL; 
+
+
+                assert(bound_high == BOUND_UNDEFINED || virtual_i < bound_high);
+                bound_high = virtual_i;
+
+                assert(bound_low == BOUND_UNDEFINED || virtual_i > bound_low);
+                bound_low = virtual_i;
+
+                low = virtual_i + 1;
+                high = virtual_i - 1;
+
+            }
+
+            // 0 1
+            // S - Gi < 0
+            // S < Gi
+            // Gi > S
+            if (!black_first && white_first)
+            {
+                grid[real_i] = R_GREATER; 
+                high = virtual_i - 1;
+
+                assert(bound_high == BOUND_UNDEFINED || virtual_i < bound_high);
+                bound_high = virtual_i;
+            }
 
             // 1 0
             // S - Gi > 0
@@ -316,84 +298,131 @@ void get_bounds(vector<game*>& games)
                 split_arena = {virtual_i + 1, high};
                 high = virtual_i - 1;
             }
-
-        }
-        */
-
-        // 2 SIDED OPTIMIZATION
-        search_result black_first = RESULT_UNKNOWN;
-        search_result white_first = RESULT_UNKNOWN;
-        
-        // What side of the bound range are we on?
-        int bound_side = virtual_i <= 0 ? -1 : 1;
-
-        if (bound_low != BOUND_UNDEFINED && bound_high != BOUND_UNDEFINED)
-        {
-            int mid = (bound_low + bound_high) / 2;
-
-            bound_side = virtual_i <= mid ? -1 : 1;
         }
 
-        assert(bound_side != 0);
 
-
-        // If we're on the lower side, assume that we'll be increasing the lower bound
-        // so check white first
-        // Otherwise check black first...
-
-
-        // First search
-        bool conclusive = false;
-
-        if (bound_side < 0)
+        // 1 SIDED OPTIMIZATION
+        if (OPTIMIZATION_LEVEL == OPT_1_SIDE)
         {
-            sum.set_to_play(WHITE);
-            white_first = (search_result) sum.solve();
             search_count++;
+            bool black_first = sum.solve();
 
-            if (white_first == false)
+            // 0 ?
+            // S - Gi <= 0
+            // S <= Gi
+            // Gi >= S
+            if (!black_first)
             {
-                conclusive = true;
-
-                // ? 0
-                // S - Gi >= 0
-                // S >= Gi
-                // Gi <= S
-                grid[real_i] = R_LE;
-                low = virtual_i + 1;
-                assert(bound_low == BOUND_UNDEFINED || virtual_i > bound_low);
-                bound_low = virtual_i;
-            }
-        } else
-        {
-            sum.set_to_play(BLACK);
-            black_first = (search_result) sum.solve();
-            search_count++;
-
-            if (black_first == false)
-            {
-                conclusive = true;
-
-                // 0 ?
-                // S - Gi <= 0
-                // S <= Gi
-                // Gi >= S
-                grid[real_i] = R_GE;
+                grid[real_i] = R_GE; 
                 high = virtual_i - 1;
 
                 assert(bound_high == BOUND_UNDEFINED || virtual_i < bound_high);
                 bound_high = virtual_i;
+            } else
+            {
+
+                search_count++;
+                sum.set_to_play(WHITE);
+                bool white_first = sum.solve();
+
+                // 1 0
+                // S - Gi > 0
+                // S > Gi
+                // Gi < S
+                if (black_first && !white_first)
+                {
+                    grid[real_i] = R_LESS;
+                    low = virtual_i + 1;
+
+                    assert(bound_low == BOUND_UNDEFINED || virtual_i > bound_low);
+                    bound_low = virtual_i;
+                }
+
+                // 1 1
+                // S - Gi ?= 0
+                // S ?= Gi
+                // Gi ?= S
+                if (black_first && white_first)
+                {
+                    grid[real_i] = R_FUZZY;
+                    split_arena = {virtual_i + 1, high};
+                    high = virtual_i - 1;
+                }
+
             }
         }
 
-
-        if (!conclusive)
+        // 2 SIDED OPTIMIZATION
+        if (OPTIMIZATION_LEVEL == OPT_2_SIDE || OPTIMIZATION_LEVEL == OPT_ASSUME_NEG || OPTIMIZATION_LEVEL == OPT_ASSUME_POS)
         {
+            static int tie_break_rule = -1;
+
+            search_result black_first = RESULT_UNKNOWN;
+            search_result white_first = RESULT_UNKNOWN;
+            
+            // What side of the bound range are we on?
+            int mid = 0;
+
+            if (bound_low != BOUND_UNDEFINED && bound_high != BOUND_UNDEFINED)
+            {
+                mid = (bound_low + bound_high) / 2;
+            }
+
+            int bound_side = tie_break_rule;
+            bool did_tie_break = true;
+
+            if (virtual_i != mid)
+            {
+                did_tie_break = false;
+                bound_side = virtual_i < mid ? -1 : 1;
+            }
+
+
+            if (OPTIMIZATION_LEVEL == OPT_ASSUME_NEG)
+            {
+                bound_side = -1;
+            }
+
+            if (OPTIMIZATION_LEVEL == OPT_ASSUME_POS)
+            {
+                bound_side = 1;
+            }
+
+
+            assert(bound_side != 0);
+
+
+            // If we're on the lower side, assume that we'll be increasing the lower bound
+            // so check white first
+            // Otherwise check black first...
+
+
+            // First search
+            bool conclusive = false;
+
+            int local_search_count = 1;
+
             if (bound_side < 0)
             {
-                assert(black_first == RESULT_UNKNOWN);
-                assert(white_first != RESULT_UNKNOWN);
+                sum.set_to_play(WHITE);
+                white_first = (search_result) sum.solve();
+                search_count++;
 
+                if (white_first == false)
+                {
+                    conclusive = true;
+
+                    // ? 0
+                    // S - Gi >= 0
+                    // S >= Gi
+                    // Gi <= S
+                    grid[real_i] = R_LE;
+                    low = virtual_i + 1;
+                    assert(bound_low == BOUND_UNDEFINED || virtual_i > bound_low);
+                    bound_low = virtual_i;
+                }
+            } else
+            {
                 sum.set_to_play(BLACK);
                 black_first = (search_result) sum.solve();
                 search_count++;
@@ -402,7 +431,7 @@ void get_bounds(vector<game*>& games)
                 {
                     conclusive = true;
 
-                    // 0 1
+                    // 0 ?
                     // S - Gi <= 0
                     // S <= Gi
                     // Gi >= S
@@ -412,42 +441,79 @@ void get_bounds(vector<game*>& games)
                     assert(bound_high == BOUND_UNDEFINED || virtual_i < bound_high);
                     bound_high = virtual_i;
                 }
-            } else
+            }
+
+
+            if (!conclusive)
             {
-                assert(black_first != RESULT_UNKNOWN);
-                assert(white_first == RESULT_UNKNOWN);
+                local_search_count++;
 
-                sum.set_to_play(WHITE);
-                white_first = (search_result) sum.solve();
-                search_count++;
-
-                if (white_first == false)
+                if (bound_side < 0)
                 {
-                    conclusive = true;
+                    assert(black_first == RESULT_UNKNOWN);
+                    assert(white_first != RESULT_UNKNOWN);
 
-                    // 1 0
-                    // S - Gi >= 0
-                    // S >= Gi
-                    // Gi <= S
-                    grid[real_i] = R_LE;
-                    low = virtual_i + 1;
-                    assert(bound_low == BOUND_UNDEFINED || virtual_i > bound_low);
-                    bound_low = virtual_i;
+                    sum.set_to_play(BLACK);
+                    black_first = (search_result) sum.solve();
+                    search_count++;
+
+                    if (black_first == false)
+                    {
+                        conclusive = true;
+
+                        // 0 1
+                        // S - Gi <= 0
+                        // S <= Gi
+                        // Gi >= S
+                        grid[real_i] = R_GE;
+                        high = virtual_i - 1;
+
+                        assert(bound_high == BOUND_UNDEFINED || virtual_i < bound_high);
+                        bound_high = virtual_i;
+                    }
+                } else
+                {
+                    assert(black_first != RESULT_UNKNOWN);
+                    assert(white_first == RESULT_UNKNOWN);
+
+                    sum.set_to_play(WHITE);
+                    white_first = (search_result) sum.solve();
+                    search_count++;
+
+                    if (white_first == false)
+                    {
+                        conclusive = true;
+
+                        // 1 0
+                        // S - Gi >= 0
+                        // S >= Gi
+                        // Gi <= S
+                        grid[real_i] = R_LE;
+                        low = virtual_i + 1;
+                        assert(bound_low == BOUND_UNDEFINED || virtual_i > bound_low);
+                        bound_low = virtual_i;
+                    }
                 }
+            }
+
+            // If still inconclusive, we have "1 1"
+
+            if (!conclusive)
+            {
+                assert(black_first == RESULT_TRUE);
+                assert(white_first == RESULT_TRUE);
+
+                grid[real_i] = R_FUZZY;
+                split_arena = {virtual_i + 1, high};
+                high = virtual_i - 1;
+            }
+
+            if (did_tie_break && conclusive && (local_search_count == 2))
+            {
+                tie_break_rule *= -1;
             }
         }
 
-        // If still inconclusive, we have "1 1"
-
-        if (!conclusive)
-        {
-            assert(black_first == RESULT_TRUE);
-            assert(white_first == RESULT_TRUE);
-
-            grid[real_i] = R_FUZZY;
-            split_arena = {virtual_i + 1, high};
-            high = virtual_i - 1;
-        }
 
 
         delete inverse_scale_game;
@@ -463,11 +529,14 @@ void get_bounds(vector<game*>& games)
     {
         arenas_next.clear();
 
-        for (const arena& ar : arenas)
+        if (!silent)
         {
-            cout << ar << endl;
+            for (const arena& ar : arenas)
+            {
+                cout << ar << endl;
+            }
+            cout << endl;
         }
-        cout << endl;
 
         for (size_t i = 0; i < arenas.size(); i++)
         {
@@ -501,48 +570,51 @@ void get_bounds(vector<game*>& games)
     //const int N_ROWS = 0;
 
 
-    for (int n = 0; n < N_ROWS; n++)
+    if (!silent)
     {
-        int min = MIN + n * ROW_COLS;
-
-        int max = min + ROW_COLS - 1;
-        max = max > MAX ? MAX : max;
-
-        for (int i = min; i <= max; i++)
+        for (int n = 0; n < N_ROWS; n++)
         {
-            cout << i << sep;
+            int min = MIN + n * ROW_COLS;
+
+            int max = min + ROW_COLS - 1;
+            max = max > MAX ? MAX : max;
+
+            for (int i = min; i <= max; i++)
+            {
+                cout << i << sep;
+            }
+            cout << endl;
+
+            for (int i = min; i <= max; i++)
+            {
+                game* scale_game = get_scale_game(i);
+
+                stringstream stream;
+                stream << *scale_game;
+                string str = stream.str();
+                remove_game_prefixes(str);
+
+                cout << str << sep;
+
+                delete scale_game;
+            }
+            cout << endl;
+
+            for (int i = min; i <= max; i++)
+            {
+                int real_i = virtual_to_real_idx(i);
+
+                relation r = grid[real_i];
+                auto it = REL_MAP.find(r);
+                assert(it != REL_MAP.end());
+                const string& text = it->second;
+
+                cout << text << sep;
+            }
+            cout << endl;
+            cout << endl;
+
         }
-        cout << endl;
-
-        for (int i = min; i <= max; i++)
-        {
-            game* scale_game = get_scale_game(i);
-
-            stringstream stream;
-            stream << *scale_game;
-            string str = stream.str();
-            remove_game_prefixes(str);
-
-            cout << str << sep;
-
-            delete scale_game;
-        }
-        cout << endl;
-
-        for (int i = min; i <= max; i++)
-        {
-            int real_i = virtual_to_real_idx(i);
-
-            relation r = grid[real_i];
-            auto it = REL_MAP.find(r);
-            assert(it != REL_MAP.end());
-            const string& text = it->second;
-
-            cout << text << sep;
-        }
-        cout << endl;
-        cout << endl;
-
     }
 
     cout << "BOUNDS: [" << bound_low << " " << bound_high << "]" << endl; 
@@ -552,37 +624,68 @@ void get_bounds(vector<game*>& games)
         Clobber:
         XOXO.XO.XOXOXO.XXOOXO
 
-        bounds should be [-1 1]
+        radius  NONE    1_SIDE  2_SIDE  NEG     POS
+        32      22      17      12      17      17
+        16000   54      41      29      41      41
 
-        basic approach:
-            radius      checks      sumgames
-            32          11          22
-            16000       27          54
 
-        1-sided optimization:
-            radius      checks      sumgames
-            32          11          17
-            16000       27          41
 
-        2-sided optimization:
-            radius      checks      sumgames
-            32          11          12
-            16000       27          29
+        Clobber:
+        XOXOXOXO.XO.XXOO.OXXO
+
+        radius  NONE    1_SIDE  2_SIDE  NEG     POS
+        32      22      18      14      18      18
+        16000   58      45      32      45      45
+
+
+        Clobber:
+        XXXXXXXO
+        radius  NONE    1_SIDE  2_SIDE  NEG     POS
+        32      10      9       8       9       9
+        16000   30      18      25      26      18
+
+
+
+
+
 
 
     */
-    cout << "Did " << check_count << " checks (" << search_count << " sumgames)" << endl;
+    cout << "Did " << step_count << " steps (" << search_count << " sumgames)" << endl;
 }
 
 
 void test_bounds()
 {
+     
 
     vector<game*> games;
 
-    games.push_back(new clobber_1xn("XOXO.XO.XOXOXO.XXOOXO"));
+    //games.push_back(new clobber_1xn("XOXO.XO.XOXOXO.XXOOXO"));
+    //games.push_back(new clobber_1xn("XOXOXOXO.XO.XXOO.OXXO"));
+    //games.push_back(new clobber_1xn("XXXXXXXO"));
+    games.push_back(new clobber_1xn("OOOOOOOX"));
 
-    get_bounds(games);
+
+    vector<optimization_level_enum> opts {
+        OPT_NONE,
+
+        OPT_1_SIDE,
+
+        OPT_2_SIDE,
+
+        OPT_ASSUME_NEG,
+        OPT_ASSUME_POS,
+    };
+
+    for (const optimization_level_enum& opt : opts)
+    {
+        step_count = 0;
+        search_count = 0;
+
+        OPTIMIZATION_LEVEL = opt;
+        get_bounds(games);
+    }
 
     for (game* g : games)
     {
