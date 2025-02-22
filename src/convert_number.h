b47e05085bfefba8ca6da2228fd6066e404911b8
tfolkersen <folkerse@ualberta.ca>
2025-02-22 01:21:59 -0700

    rough "simplest number" algorithm

---
+++ src/convert_number.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <cgt_dyadic_rational.h>
+
+void test_convert_number();
+
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -15,11 +15,15 @@
 #include <chrono>
 
 #include "all_game_headers.h"
+#include "convert_number.h"
 
 using std::cout, std::endl, std::string;
 
 int main(int argc, char** argv)
 {
+    test_convert_number();
+    return 0;
+
     cli_options opts = parse_cli_args(argc, (const char**) argv, false);
 
     // i.e. ./MCGS --help
