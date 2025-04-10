0e3087bd3b395c6439c85bf045063a24292ae62f
Martin Müller <mmueller@ualberta.ca>
2025-04-10 15:22:33 -0600

    Prepare for testing impartial_sumgame

---
+++ test/impartial_sumgame_test.cpp
@@ -0,0 +1,10 @@
+//---------------------------------------------------------------------------
+// Unit tests for impartial_sumgame
+// Compute nim_value and win/loss minimax result
+//---------------------------------------------------------------------------
+#include "impartial_sumgame_test.h"
+#include "impartial_sumgame.h"
+
+void impartial_sumgame_test_all()
+{
+}
