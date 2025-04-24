acd5dea863b1ff70a5a09f0b1e8346f59d6fdb2c
tfolkersen <folkerse@ualberta.ca>
2025-04-23 23:11:24 -0600

    Ensure AddressSanitizer features are enabled for Mac OS etc

---
+++ src/asan_options.cpp
@@ -0,0 +1,12 @@
+/*
+    Default settings for AddressSanitizer; some of these may not be enabled
+    by default on Mac OS
+
+    extern "C" needed because of C++ name mangling?
+*/
+
+// NOLINTNEXTLINE(readability-identifier-naming,misc-use-internal-linkage)
+extern "C" const char* __asan_default_options() {
+    return "detect_stack_use_after_return=1:detect_leaks=1";
+}
+
