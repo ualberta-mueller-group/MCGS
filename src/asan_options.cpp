8e0fddf3233378c8e7629c5d574e3ad9774f0faf
tfolkersen <folkerse@ualberta.ca>
2025-04-24 03:05:40 -0600

    add ASAN check for global initialization order dependencies

--- src/asan_options.cpp
+++ src/asan_options.cpp
@@ -1,14 +1,26 @@
 /*
-    Default settings for AddressSanitizer; some of these may not be enabled
-    by default on Mac OS
+    Settings for AddressSanitizer; some of these may not be enabled
+    by default, especially on Mac OS
 
-    extern "C" needed because of C++ name mangling?
+    The function defined by this file is used by external tooling, when
+    compiling with the -fsanitize flag.
 
     See: https://github.com/google/sanitizers/wiki/AddressSanitizerFlags
+
+    extern "C" needed because of C++ name mangling?
 */
 
 // NOLINTNEXTLINE(readability-identifier-naming,misc-use-internal-linkage)
 extern "C" const char* __asan_default_options() {
-    return "detect_stack_use_after_return=1:detect_leaks=1";
+    // string literals automatically concatenated
+
+    return
+        "detect_stack_use_after_return=1"
+
+        ":detect_leaks=1"
+
+        // Catch global variable initialization order problems
+        ":check_initialization_order=1"
+        ":strict_init_order=1";
 }
 
