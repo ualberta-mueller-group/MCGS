816dffa15f6950ec67442fbdabc3a2df9bceccdf
tfolkersen <folkerse@ualberta.ca>
2025-06-21 20:19:42 -0600

    most of dynamic_serializer<T>

--- src/serializer.h
+++ src/serializer.h
@@ -11,14 +11,14 @@
 
 ////////////////////////////////////////////////// serialize<T>
 template <class T, class Enable = void>
-struct serialize
+struct serializer
 {
     static_assert(false, "Not implemented!");
 };
 
 //////////////////////////////////////// integral types
 template <class T>
-struct serialize<T,
+struct serializer<T,
     std::enable_if_t<
         std::is_integral_v<T>,
         void
@@ -40,7 +40,7 @@ struct serialize<T,
 
 //////////////////////////////////////// vector<T>
 template <class T>
-struct serialize<std::vector<T>>
+struct serializer<std::vector<T>>
 {
     inline static void save(obuffer& os, const std::vector<T>& val)
     {
@@ -48,7 +48,7 @@ struct serialize<std::vector<T>>
         os.write_u64(size);
 
         for (size_t i = 0; i < size; i++)
-            serialize<T>::save(os, val[i]);
+            serializer<T>::save(os, val[i]);
     }
 
     inline static std::vector<T> load(ibuffer& is)
@@ -59,7 +59,7 @@ struct serialize<std::vector<T>>
         vec.reserve(size);
 
         for (uint64_t i = 0; i < size; i++)
-            vec.emplace_back(serialize<T>::load(is));
+            vec.emplace_back(serializer<T>::load(is));
 
         return vec;
     }
