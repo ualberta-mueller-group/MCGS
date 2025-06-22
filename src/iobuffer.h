01bd419a6736f2665ba59b19958fce376880c874
tfolkersen <folkerse@ualberta.ca>
2025-06-21 18:55:01 -0600

    add iobuffer.h

---
+++ src/iobuffer.h
@@ -0,0 +1,138 @@
+#pragma once
+#include <climits>
+#include <string>
+#include <type_traits>
+#include <cassert>
+#include <fstream>
+#include <cstdint>
+#include <iostream>
+
+// TODO: assert or THROW_ASSERT for this file?
+
+
+// TODO remove this:
+// NOLINTBEGIN(readability-identifier-naming)
+
+////////////////////////////////////////////////// fmt_write/fmt_read
+
+// Pass T by value, not reference (avoid size mismatch)
+template <class T>
+inline void __fmt_write(std::ostream& os, T val)
+{
+    static_assert(CHAR_BIT == 8); // 8 bits per byte
+    static_assert(std::is_integral_v<T>);
+
+    using T_Unsigned = std::make_unsigned_t<T>;
+    const T_Unsigned& val_uns = reinterpret_cast<const T_Unsigned&>(val);
+
+    constexpr unsigned int size = sizeof(T);
+
+    for (unsigned int i = 0; i < size; i++)
+    {
+        assert(os);
+
+        uint8_t byte = (uint8_t) (val_uns >> (i * 8));
+        os << byte;
+    }
+
+    assert(os);
+}
+
+template <class T>
+inline T __fmt_read(std::istream& is)
+{
+    static_assert(CHAR_BIT == 8); // 8 bits per byte
+    static_assert(std::is_integral_v<T>);
+
+    constexpr unsigned int size = sizeof(T);
+
+    T val(0);
+    uint8_t byte;
+
+    for (unsigned int i = 0; i < size; i++)
+    {
+        assert(is);
+
+        is >> byte;
+        const T byte_longer = byte;
+        val |= (byte_longer << (i * 8));
+    }
+
+    assert(is);
+
+    return val;
+}
+
+
+////////////////////////////////////////////////// obuffer/ibuffer
+class obuffer
+{
+public:
+    obuffer(const std::string& file_name): _fs(file_name, OPEN_MODE)
+    {
+        assert(_fs.is_open());
+    }
+
+    virtual ~obuffer()
+    {
+    }
+
+    inline void close()
+    {
+        assert(_fs.is_open());
+        _fs.close();
+        assert(!_fs.is_open());
+    }
+
+    void write_u8(const uint8_t& val);
+    void write_u16(const uint16_t& val);
+    void write_u32(const uint32_t& val);
+    void write_u64(const uint64_t& val);
+    void write_i8(const int8_t& val);
+    void write_i16(const int16_t& val);
+    void write_i32(const int32_t& val);
+    void write_i64(const int64_t& val);
+
+private:
+    static constexpr std::ofstream::openmode OPEN_MODE = //
+        std::ofstream::binary |                          //
+        std::ofstream::trunc;                            //
+
+    std::ofstream _fs;
+};
+
+class ibuffer
+{
+public:
+    ibuffer(const std::string& file_name): _fs(file_name, OPEN_MODE)
+    {
+        assert(_fs.is_open());
+    }
+
+    virtual ~ibuffer()
+    {
+    }
+
+    inline void close()
+    {
+        assert(_fs.is_open());
+        _fs.close();
+        assert(!_fs.is_open());
+    }
+
+    uint8_t read_u8();
+    uint16_t read_u16();
+    uint32_t read_u32();
+    uint64_t read_u64();
+    int8_t read_i8();
+    int16_t read_i16();
+    int32_t read_i32();
+    int64_t read_i64();
+
+private:
+    static constexpr std::ifstream::openmode OPEN_MODE = std::ifstream::binary;
+
+    std::ifstream _fs;
+};
+
+// NOLINTEND(readability-identifier-naming)
