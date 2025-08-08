c178f365f3fee76f6e42335b3e584bdb2cbf41db
tfolkersen <folkerse@ualberta.ca>
2025-08-07 21:17:35 -0600

    add flags

--- src/global_options.h
+++ src/global_options.h
@@ -163,6 +163,10 @@ extern global_option<bool> use_db;
 extern global_option<bool> clear_tt;
 extern global_option<uint64_t> experiment_seed;
 
+extern global_option<bool> play_split;
+extern global_option<bool> play_normalize;
+extern global_option<bool> dedupe_movegen;
+
 extern global_option<bool> silence_warnings;
 extern global_option<bool> print_ttable_size;
 
