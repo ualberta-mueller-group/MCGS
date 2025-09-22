6be831d80e8cc15325a9a220bc8891f3a00a7a80
tfolkersen <folkerse@ualberta.ca>
2025-09-22 01:30:11 -0600

    add notes

--- src/grid_hash.cpp
+++ src/grid_hash.cpp
@@ -22,6 +22,33 @@ void print_grid(const int_pair& shape, const vector<int>& vec)
 
 } // namespace
 
+////////////////////////////////////////////////// grid_hash methods
+void grid_hash::reset(const int_pair& grid_shape)
+{
+    assert(grid_shape.first >= 0 && grid_shape.second >= 0);
+
+    _grid_shape = grid_shape;
+
+    constexpr size_t N_ROTATIONS = GRID_HASH_ROTATIONS.size();
+    for (size_t rot_idx = 0; rot_idx < N_ROTATIONS; rot_idx++)
+    {
+        const grid_hash_rotation rot = GRID_HASH_ROTATIONS[rot_idx];
+        const int_pair shape = _get_rotated_shape(rot);
+
+        const size_t i = rot_idx * 2;
+        local_hash& hash_normal = _hashes[i];
+        local_hash& hash_transpose = _hashes[i + 1];
+
+        hash_normal.reset();
+        hash_normal.toggle_value(0, shape.first);
+        hash_normal.toggle_value(1, shape.second);
+
+        hash_transpose.reset();
+        hash_transpose.toggle_value(0, shape.second);
+        hash_transpose.toggle_value(1, shape.first);
+    }
+}
+
 //////////////////////////////////////////////////
 void test_grid_hash_stuff()
 {
--- src/grid_hash.h
+++ src/grid_hash.h
@@ -1,5 +1,22 @@
 #pragma once
 
+/*
+    grid_hash::_hashes array has 8 local_hash objects
+
+    0, 0T, 90, 90T, 180, 180T, 270, 270T
+
+Disable some with bit mask? Have common bit masks?
+
+All:
+    nogo, clobber, amazons
+
+0, 180, 90T, 270T:
+    domineering, fission
+
+Won't ever use?:
+    clobber_1xn, toppling_dominoes, nogo_1xn, elephants, gen_toads
+*/
+
 #include <algorithm>
 #include <cstddef>
 #include <array>
@@ -105,31 +122,6 @@ private:
 ////////////////////////////////////////////////// grid_hash methods
 // TODO don't inline these all...
 
-inline void grid_hash::reset(const int_pair& grid_shape)
-{
-    assert(grid_shape.first >= 0 && grid_shape.second >= 0);
-
-    _grid_shape = grid_shape;
-
-    constexpr size_t N_ROTATIONS = GRID_HASH_ROTATIONS.size();
-    for (size_t rot_idx = 0; rot_idx < N_ROTATIONS; rot_idx++)
-    {
-        const grid_hash_rotation rot = GRID_HASH_ROTATIONS[rot_idx];
-        const int_pair shape = _get_rotated_shape(rot);
-
-        const size_t i = rot_idx * 2;
-        local_hash& hash_normal = _hashes[i];
-        local_hash& hash_transpose = _hashes[i + 1];
-
-        hash_normal.reset();
-        hash_normal.toggle_value(0, shape.first);
-        hash_normal.toggle_value(1, shape.second);
-
-        hash_transpose.reset();
-        hash_transpose.toggle_value(0, shape.second);
-        hash_transpose.toggle_value(1, shape.first);
-    }
-}
 
 // TODO why not std::min_element with em++?
 inline hash_t grid_hash::get_value() const
