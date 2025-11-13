f953ff7ae8da6bca3118bc0dd26fe4838b1027d2
Martin Müller <mmueller@ualberta.ca>
2025-11-13 12:22:39 -0700

    make winning_moves game-independent

--- src/winning_moves.cpp
+++ src/winning_moves.cpp
@@ -51,36 +51,27 @@ std::vector<move> get_winning_moves(const game* g, bw to_play)
 void print_winning_moves_impl(std::shared_ptr<file_parser> fp)
 {
     game_case gc;
-    bool is_first = true;
 
     while (fp->parse_chunk(gc))
     {
         THROW_ASSERT(gc.games.size() == 1 && is_black_white(gc.to_play));
 
         game* g = gc.games.back();
-        THROW_ASSERT(dynamic_cast<sheep*>(g) != nullptr);
+        THROW_ASSERT(g != nullptr);
 
-        std::vector<move> winning_moves = get_winning_moves(g, gc.to_play);
-
-        if (is_first)
-            is_first = false;
+        std::cout << *g << '\n' << "Winning moves: ";
+        const std::vector<move> winning_moves = get_winning_moves(g, gc.to_play);
+        if (winning_moves.empty())
+            std::cout << "None";
         else
-            std::cout << std::endl;
-
-        std::cout << *g << std::endl;
-
-        for (move m : winning_moves)
         {
-            unsigned int from_point;
-            unsigned int to_point;
-            unsigned int target_herd_abs;
-
-            cgt_move::decode_three_part_move(m, from_point, to_point,
-                                             target_herd_abs);
-
-            std::cout << from_point << " -> " << to_point << " ("
-                      << target_herd_abs << ")" << std::endl;
+            for (move m : winning_moves)
+            {
+                g->print_move(std::cout, m);
+                std::cout << ' ';
+            }
         }
+        std::cout << std::endl;
 
         gc.cleanup_games();
     }
