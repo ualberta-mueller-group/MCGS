a2f4c1dd26616bd90832b68c9c6621bf5eabbfe0
tfolkersen <folkerse@ualberta.ca>
2025-11-30 05:10:41 -0700

    remove --alt_imp_search for now

--- src/global_options.h
+++ src/global_options.h
@@ -163,7 +163,7 @@ extern global_option<bool> use_db;
 extern global_option<bool> clear_tt;
 extern global_option<bool> count_sums;
 extern global_option<uint64_t> experiment_seed;
-extern global_option<bool> alt_imp_search;
+//extern global_option<bool> alt_imp_search;
 
 extern global_option<bool> play_normalize;
 extern global_option<bool> dedupe_movegen;
