1a0333787a910828a6c1eed6f5164e5efa7c1222
Martin Müller <mmueller@ualberta.ca>
2025-12-10 10:38:55 -0700

    Add documentation; cleanup

--- src/pitm_move_generator.h
+++ src/pitm_move_generator.h
@@ -1,17 +1,20 @@
 /*
-    Unused move_generator wrapper implemented play-in-the-middle heuristic.
+    Wrapper for move_generator. Implements play-in-the-middle heuristic.
 
-    Generates all moves from the move_generator during construction, and stores
-    them in memory
+    Generates all moves from the move_generator during construction, 
+    stores them as a list,
+    then pre-computes the pitm-list from the middle of the move list 
+    outwards towards the ends.
 */
 #pragma once
-#include "game.h"
-#include <vector>
 #include <cstddef>
+#include <vector>
+#include "game.h"
 
 class pitm_move_generator : public move_generator
 {
 public:
+    // assumes ownership of gen.
     pitm_move_generator(move_generator* gen, bw to_play);
 
     virtual ~pitm_move_generator() {}
