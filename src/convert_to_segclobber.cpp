352473aa6f2e8bd20174ddac1cf492cc0c3009ad
tfolkersen <folkerse@ualberta.ca>
2026-03-22 22:54:21 -0600

    convert BW to XO, finish conversion functions

--- src/convert_to_segclobber.cpp
+++ src/convert_to_segclobber.cpp
@@ -3,12 +3,9 @@
 #include <cassert>
 #include <filesystem>
 #include <ostream>
-#include <sstream>
-#include <system_error>
 
 #include "cgt_basics.h"
 #include "clobber_1xn.h"
-#include "file_iterator.h"
 #include "file_parser_ast.h"
 #include "game.h"
 #include "test_case.h"
@@ -20,12 +17,42 @@ using namespace std;
 
 ////////////////////////////////////////////////// Helpers
 namespace {
-inline char color_to_segclobber_char(int color)
+inline char color_to_segclobber_char(ebw color)
 {
     switch (color)
     {
         case EMPTY:
             return '.';
+        case BLACK:
+            return 'X';
+        case WHITE:
+            return 'O';
+        default:
+            THROW_ASSERT(false);
+    }
+}
+
+inline ebw segclobber_char_to_color(char c)
+{
+    switch (c)
+    {
+        case '.':
+            return EMPTY;
+        case 'X':
+            return BLACK;
+        case 'O':
+            return WHITE;
+        default:
+            THROW_ASSERT(false);
+    }
+}
+
+inline char color_to_segclobber_player_char(ebw color)
+{
+    switch (color)
+    {
+        case EMPTY:
+            return '?';
         case BLACK:
             return 'B';
         case WHITE:
@@ -35,11 +62,12 @@ inline char color_to_segclobber_char(int color)
     }
 }
 
-inline char segclobber_char_to_color(char c)
+#if false
+inline ebw segclobber_player_char_to_color(char c)
 {
     switch (c)
     {
-        case '.':
+        case '?':
             return EMPTY;
         case 'B':
             return BLACK;
@@ -49,16 +77,17 @@ inline char segclobber_char_to_color(char c)
             THROW_ASSERT(false);
     }
 }
+#endif
 
 /*
-    Returns string of 'B', 'W', and '.', obtained by concatenating the given
+    Returns string of 'X', 'O', and '.', obtained by concatenating the given
     clobber_1xn boards. If a given game is not of type clobber_1xn, returns no
     value.
 */
 optional<string> make_segclobber_game_string(const vector<game*>& games)
 {
     string game_string;
-    
+
     auto last_is_empty = [&]() -> bool
     {
         if (game_string.empty())
@@ -81,12 +110,16 @@ optional<string> make_segclobber_game_string(const vector<game*>& games)
         if (!last_is_empty())
             game_string.push_back(color_to_segclobber_char(EMPTY));
 
+        assert(last_is_empty());
         for (int tile : board)
         {
             const char c = color_to_segclobber_char(tile);
             game_string.push_back(c);
         }
     }
+
+    if (game_string.empty())
+        game_string.push_back(color_to_segclobber_char(EMPTY));
 
     return game_string;
 }
@@ -109,30 +142,31 @@ pair<string, string> extract_player_and_expected_winner(
 
     const minimax_outcome_enum expected_outcome = expr.get_expected_outcome();
 
-    char expected_winner;
+    ebw expected_winner;
     switch (expected_outcome)
     {
         case MINIMAX_OUTCOME_NONE:
         {
-            expected_winner = '?';
+            expected_winner = EMPTY;
             break;
         }
         case MINIMAX_OUTCOME_WIN:
         {
-            expected_winner = color_to_segclobber_char(player);
+            expected_winner = player;
             break;
         }
         case MINIMAX_OUTCOME_LOSS:
         {
-            expected_winner = color_to_segclobber_char(opponent(player));
+            expected_winner = opponent(player);
             break;
         }
         default:
             assert(false);
     }
 
-    player_and_winner_pair.first = color_to_segclobber_char(player);
-    player_and_winner_pair.second = expected_winner;
+    player_and_winner_pair.first = color_to_segclobber_player_char(player);
+    player_and_winner_pair.second =
+        color_to_segclobber_player_char(expected_winner);
 
     assert(!player_and_winner_pair.first.empty() &&
            !player_and_winner_pair.second.empty());
@@ -150,16 +184,20 @@ ofstream open_output_file(const string& output_dir_name,
     const std::filesystem::path output_file_path(output_dir_path /
                                                  output_file_name);
 
-    // Make directories if they don't exist
-    if (!std::filesystem::exists(output_dir_path))
-        std::filesystem::create_directories(output_dir_path);
+    const std::filesystem::path output_file_path_parent =
+        output_file_path.parent_path();
 
-    const std::filesystem::path output_file_path_parent = output_file_path.parent_path();
+    THROW_ASSERT(!std::filesystem::exists(output_file_path),
+                 "File \"" + output_file_path.string() + "\" already exists!");
+
+    // Make directories if they don't exist
     if (!std::filesystem::exists(output_file_path_parent))
         std::filesystem::create_directories(output_file_path_parent);
 
-    THROW_ASSERT(std::filesystem::exists(output_dir_path) &&
-                 std::filesystem::is_directory(output_dir_path));
+    THROW_ASSERT(std::filesystem::exists(output_file_path_parent) &&
+                     std::filesystem::is_directory(output_file_path_parent),
+                 "Couldn't open directory \"" +
+                     output_file_path_parent.string() + "\"!");
 
     ofstream of(output_file_path);
     THROW_ASSERT(of.is_open());
@@ -178,6 +216,7 @@ optional<string> convert_test_single(const test_case_solve_bw* test_case)
         return {};
 
     const string& game_string = game_string_opt.value();
+    assert(!game_string.empty());
 
     pair<string, string> player_and_winner =
         extract_player_and_expected_winner(test_case);
@@ -193,10 +232,20 @@ optional<string> convert_test_single(const test_case_solve_bw* test_case)
     test_line += player_and_winner.first;
     test_line += ' ';
 
-    test_line += player_and_winner.second ;
+    test_line += player_and_winner.second;
     test_line += '\n';
 
     return test_line;
+}
+
+// Either the target doesn't exist, or is an empty directory
+bool output_dir_is_valid(const string& output_dir_name)
+{
+    if (!std::filesystem::exists(output_dir_name))
+        return true;
+
+    return std::filesystem::is_directory(output_dir_name) &&
+           std::filesystem::is_empty(output_dir_name);
 }
 
 } // namespace
@@ -205,9 +254,13 @@ optional<string> convert_test_single(const test_case_solve_bw* test_case)
 void convert_tests_to_segclobber(shared_ptr<file_parser> fp,
                                  const string& output_dir_name)
 {
+    THROW_ASSERT(output_dir_is_valid(output_dir_name),
+                 "Output path \"" + output_dir_name +
+                     "\" exists and is not an empty directory!");
+
     ofstream output_file =
         open_output_file(output_dir_name, "segclobber-tests.txt");
-    assert(output_file.is_open());
+    THROW_ASSERT(output_file.is_open());
 
     uint64_t n_skipped_tests = 0;
 
@@ -227,7 +280,8 @@ void convert_tests_to_segclobber(shared_ptr<file_parser> fp,
                 dynamic_cast<const test_case_solve_bw*>(test_case.get());
             assert(test_case_casted != nullptr);
 
-            const optional<string> test_line = convert_test_single(test_case_casted);
+            const optional<string> test_line =
+                convert_test_single(test_case_casted);
 
             if (!test_line.has_value())
             {
@@ -253,6 +307,10 @@ void convert_tests_to_segclobber(const string& test_directory,
     THROW_ASSERT(!test_directory.empty() && !output_dir_name.empty());
     THROW_ASSERT(std::filesystem::is_directory(test_directory));
 
+    THROW_ASSERT(output_dir_is_valid(output_dir_name),
+                 "Output path \"" + output_dir_name +
+                     "\" exists and is not an empty directory!");
+
     uint64_t n_skipped_tests = 0;
 
     for (test_file_iterator iter(test_directory); iter; ++iter)
@@ -268,12 +326,11 @@ void convert_tests_to_segclobber(const string& test_directory,
         relative_file_path.replace_extension(".txt");
 
         unique_ptr<file_parser> fp(file_parser::from_file(file_path));
+        optional<ofstream> output_file;
 
         while (fp->parse_chunk())
         {
             const int n_chunk_tests = fp->n_test_cases();
-
-            optional<ofstream> output_file;
 
             for (int i = 0; i < n_chunk_tests; i++)
             {
@@ -296,14 +353,12 @@ void convert_tests_to_segclobber(const string& test_directory,
                 }
 
                 if (!output_file.has_value())
-                    output_file = open_output_file(output_dir_name, relative_file_path);
+                    output_file =
+                        open_output_file(output_dir_name, relative_file_path);
 
                 assert(output_file->is_open());
                 *output_file << *test_line << flush;
             }
-
-            if (output_file.has_value())
-                output_file->close();
         }
     }
 
@@ -311,5 +366,4 @@ void convert_tests_to_segclobber(const string& test_directory,
     cout << n_skipped_tests
          << " test case(s) skipped due to having non clobber_1xn games.";
     cout << endl;
-
 }
