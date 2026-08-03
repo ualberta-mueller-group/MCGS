b0fef6202d60dc7f52b72fb500abad87c1490dde
tfolkersen <folkerse@ualberta.ca>
2026-08-03 03:23:07 -0600

    update version number

--- src/version_info.h
+++ src/version_info.h
@@ -1,6 +1,6 @@
 #pragma once
 
 // file_parser checks for a version command when reading from file or stdin
-#define FILE_PARSER_VERSION_STRING "version 1.6"
+#define FILE_PARSER_VERSION_STRING "version 1.7"
 
-#define MCGS_VERSION_STRING "MCGS v1.6"
+#define MCGS_VERSION_STRING "MCGS v1.7"
